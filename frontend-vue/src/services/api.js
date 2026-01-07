f21987ad068cf80c586d061233ffa27fb5f6ffda
Anushka Singh <m9anushka@gmail.com>
2026-01-07 14:04:28 +0530

    updated

--- frontend-vue/src/services/api.js
+++ frontend-vue/src/services/api.js
@@ -1,7 +1,7 @@
 // frontend-vue/src/services/api.js
 import axios from 'axios'
 
-const BASE_URL = 'https://complaint-triage-backend-env.eba-j3zfgypt.ap-south-1.elasticbeanstalk.com'
+const BASE_URL = 'http://127.0.0.1:8000'
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
